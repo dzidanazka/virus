b0d00ee2bc8ab0dc9650756063d50db220922cf2
dzidanazka <92294353+dzidanazka@users.noreply.github.com>
2021-10-11 14:35:58 +0700

    Virus.py
    Try This Tools Virus

---
+++ Virus.py
@@ -0,0 +1,66 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+
+Login = raw_input("username : ")
+passw = raw_input("password : ")
+print "Veriycation Account"
+print "Wait Until Done"
+time.sleep(5)
+print
+print "Welcome"
+time.sleep(5)
+print
+print
+print
+print "Welcome To My Tools"
+time.sleep(5)
+print
+print
+print "Author : whoami"
+time.sleep(3)
+print "Team   : No Team"
+time.sleep(4)
+print
+print
+print
+print
+print "Virus : TrojansVirus,WannaCryVirus,LoveVirus,BadVirus."
+time.sleep(2)
+print 
+
+No = raw_input("Nomor : ")
+Jenis = raw_input("Virus : ")
+print "Attack Starting"
+time.sleep(5)
+print "[                  ] 0%"
+time.sleep(2)
+print "[=====             ] 25%"
+time.sleep(5)
+print "[==========        ] 50%"
+time.sleep(6)
+print "[==============    ] 75%"
+time.sleep(4)
+print "[==================] 100%"
+time.sleep(10)
+#Code Done
+#Code End
+#Code whoami
+
+def foo():
+    doc = "The foo property."
+    def fget(self):
+        return self._foo
+    def fset(self, value): 
+        self._foo = value
+    def fdel(self): 
+        del self._foo
+    return locals()
+foo = property(**foo())
+
+
+
+print "Attack Done"
